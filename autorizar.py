8a4099b2e18e9d20047eebe40a3c37ab1c3bd83f
foxcarlos <foxcarlos@gmail.com>
2013-05-15 16:16:30 -0430

    Agregando autorizacion

--- autorizar.py
+++ autorizar.py
@@ -4,3 +4,7 @@ consumer_secret = "JQUF9IFiFOpzjpTKYxsbKl5QV6o0baoD37fxFpBEE"
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth_url = auth.get_authorization_url()
 print auth_url
+verificar = auth.get_access_token(9091786)
+print "access_key = %s" %auth.acces_token.key
+print "access_secret = %s" %auth.acces_token.secret
+
