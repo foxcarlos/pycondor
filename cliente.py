48ad3ba086c222b9e2fc5685a3c83395da577c4e
foxcarlos <foxcarlos@gmail.com>
2013-10-04 12:10:54 -0430

    modified:   cliente.py

--- cliente.py
+++ cliente.py
@@ -7,7 +7,7 @@ import socket
 s = socket.socket()
 s.connect(("10.121.3.60", 8002))
 
-nombrePC = socket.gethostbyname_ex(s.gethostname())
+nombrePC = socket.gethostbyname_ex(socket.gethostname())
 comando = []
 envioInicial = nombrePC
 devuelve = []
