c17fecd894f08df3cd5d496f5df34b2d8d9c8ac0
FoxCarlos Trabajo <foxcarlos@gmail.com>
2014-07-19 16:14:52 -0430

    new file:   img/CONDOR.JPG 	new file:   matarProcesos.py 	modified:   pycondor.cfg

---
+++ matarProcesos.py
@@ -0,0 +1,19 @@
+import subprocess
+
+#ruta = '/home/shc/'
+ruta = '/home/cgarcia/'
+carpeta = raw_input('Ingrese la carpeta a Matar:')
+rutaYCarpeta = ruta+carpeta
+
+print(rutaYCarpeta)
+comando = subprocess.Popen(['lsof', '+D', rutaYCarpeta], stderr=subprocess.PIPE, stdout=subprocess.PIPE)
+lista = [f for f in comando.stdout.xreadlines()]
+lista2 = [j.split() for j in lista]
+
+for j in lista2:
+    pid = j[1]
+    if pid.isdigit():
+        print('Ejecutar KILL al proceso {0} '.format(pid))
+        comandoKill = subprocess.Popen(['kill', '-9', pid], stderr=subprocess.PIPE, stdout=subprocess.PIPE)
+    else:
+        print('No es un proceso PID')
