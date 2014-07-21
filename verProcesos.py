c8f8c9f865687acf6eee2efd3e983e5d41d2b3af
FoxCarlos Trabajo <foxcarlos@gmail.com>
2014-07-21 11:57:14 -0430

    se creo un proceso aparte para visualizar solamente

---
+++ verProcesos.py
@@ -0,0 +1,22 @@
+import subprocess
+
+carpeta = raw_input('Ingrese la archivo o carpeta a visualizar:')
+comandoLinux = "lsof | grep {0}".format(carpeta)
+
+try:
+    resultado = subprocess.check_output(comandoLinux, shell=True)
+    lista2 = [f.split() for f in resultado.split('\n')]
+
+    for j in lista2:
+        pid = j[1]
+        pidName = j[8]
+
+        if pid.isdigit():
+            print(pid, pidName)
+            #print('Ejecutar KILL al proceso {0} '.format(pid))
+            #print('Nombre del Proceso {0} '.format(pidName))
+        else:
+            print('{0} No es un proceso PID'.format(pid))
+except:
+    print('No se encontro ningun proceso con ese nombre:{0}'.format(carpeta))
+
